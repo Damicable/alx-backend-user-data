6e0fe303f382478259c66b82c990d2e33f4cc617
Damicable <annobidiegwu@gmail.com>
2024-02-08 14:21:55 +0000

    Connection secure

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -58,16 +58,17 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Function to implement db conectiviy"""
-    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
-    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.environ.get("PERSONAL_DATA_DB_NAME", "")
+    connect = mysql.connector.connect(
         host=host,
         database=db_name,
         user=username,
-        password=psw)
-    return conn
+        password=password
+    )
+    return connect
 
 
 def main() -> None:
