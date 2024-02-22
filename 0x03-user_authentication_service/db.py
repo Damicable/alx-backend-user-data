68c62abef5f9fad0ca4f7afea44ff7e469ce1fe2
Damicable <annobidiegwu@gmail.com>
2024-02-22 08:54:57 +0000

    Create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,62 +1,72 @@
 #!/usr/bin/env python3
-""" DB module
+"""DB module
 """
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
-from typing import TypeVar
-
-VALID_FIELD = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+from typing import Dict
 
 
 class DB:
-    """ DB class"""
+    """DB class
+    """
 
-    def __init_-(self):
-        """DB constructor"""
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self):
-        """_session method"""
+    def _session(self) -> Session:
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add_user function"""
-        if not email or not hashed_password:
-            return
-        if not email or not hashed_password:
-        session = self._session
-        session.add(user)
-        session.commit()
-        return user
-
-    def find_user_by(self, **kwargs) -> User:
-        """find_user_by function."""
-        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
-            raise InvalidRequestError
-        session = self._session
+        """saves the user to the database"""
+        DBSession = self._session
         try:
-            return session.query(User).filter_by(**kwargs).one()
+            new_user = User(email=email, hashed_password=hashed_password)
+            DBSession.add(new_user)
+            DBSession.commit()
         except Exception:
-            raise NoResultFound
+            DBSession.rollback()
 
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """update_user function"""
-        session = self._session
-        user = self.find_user_by(id=user_id)
-        for k, v in kwargs.items():
-            if k not in VALID_FIELDS:
+        return new_user
+
+    def find_user_by(self, **kwa) -> User:
+        """find_user_by function"""
+
+        try:
+            user = self._session.query(User).filter_by(**kwa).first()
+            if user is None:
+                raise NoResultFound()
+            return user
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+
+    def update_user(self, user_id: int, **kwa) -> None:
+        """updates a user"""
+
+        user_to_update = self.find_user_by(id=user_id)
+        if user_to_update is None:
+            return
+        for key, value in kwa.items():
+            if not hasattr(user_to_update, key):
                 raise ValueError
-            setattr(user, k, v)
-        session.commit()
+            setattr(user_to_update, key, value)
+
+        self.__session.commit()
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
-""" User authentication module"""
+"""User authentication module"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-base = declarative_base()
+Base = declarative_base()
 
 
-claser User(Base):
-    """ User class"""
+class User(Base):
+    """User class model"""
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(200), nullable=False)
-    hashed_passsword = Column(String(200), nullable=False)
-    session_id = Column(String(200), nullable=True)
-    reset_token = Column(String(200), nullable=True)
-
-    def __repr__(self):
-        """ String representation"""
-        return f"User: id={self.id}"
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
